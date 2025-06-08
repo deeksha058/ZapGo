3ff9fa5c184d2dbc8489340defff816c6e5f8902
deeksha <deeksha.jindal@olik.ai>
2025-06-08 21:00:15 +0530

    ZapGo Application initilised

---
+++ src/main/java/ZapGo/App/ZapGo/ZapGoApplication.java
@@ -0,0 +1,14 @@
+package ZapGo.App.ZapGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapGoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ZapGo Application Running!");
+		SpringApplication.run(ZapGoApplication.class, args);
+	}
+
+}
