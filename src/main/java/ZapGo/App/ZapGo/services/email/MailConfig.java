a7abbea3795934bf7b8c4836706407d7e259f835
deeksha058 <deekshajindal058@gmail.com>
2025-06-13 12:16:22 +0530

    fix

--- src/main/java/ZapGo/App/ZapGo/services/email/MailConfig.java
+++ src/main/java/ZapGo/App/ZapGo/services/email/MailConfig.java
@@ -1,2 +1,30 @@
-package ZapGo.App.ZapGo.services.email;public class MailConfig {
+package ZapGo.App.ZapGo.services.email;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(
+            @Value("${spring.mail.host}") String host,
+            @Value("${spring.mail.port}") int port,
+            @Value("${spring.mail.username}") String user,
+            @Value("${spring.mail.password}") String pass) {
+
+        var sender = new JavaMailSenderImpl();
+        sender.setHost(host);
+        sender.setPort(port);
+        sender.setUsername(user);
+        sender.setPassword(pass);
+
+        var props = sender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        return sender;
+    }
 }
